75807d971904dfc9b61d408554a60fba9c9561ff
Ferjs3 <101979907+Ferjs3@users.noreply.github.com>
2026-09-12 12:29:19 -0300

    f

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://amigo-invisible-backend-eg67.onrender.com',
+  apiUrl: 'https://amigo-invisible-backend-eg67.onrender.com/api',
 };
